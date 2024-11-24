fa02ddcd7dd6f730f4b16c93135fb129eb6e05e5
Marcelo <dennermarcelopereira@gmail.com>
2024-11-24 14:24:39 -0300

    adicionando usuarios

---
+++ app.js
@@ -0,0 +1,19 @@
+import sqlite3 from 'sqlite3';
+import {open} from 'sqlite';
+
+
+async function criarPopularTabelaUsuarios(nome, sobrenome) {
+    const db = await open({
+        filename: './banco.db',
+        driver: sqlite3.Database,
+    });
+
+    db.run(`CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY, nome TEXT, sobrenome TEXT)`);
+
+    db.run(`INSERT INTO usuarios (nome, sobrenome) VALUES (?,?)`, [
+        nome, 
+        sobrenome,
+    ]);
+}
+
+criarPopularTabelaUsuarios('Sandra', 'falcão');
